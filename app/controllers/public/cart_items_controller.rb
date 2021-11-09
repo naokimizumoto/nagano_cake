b8306b35105d2f4d9759985cfc59208fd2a508fe
naokimizumoto <shuibenshangxi@gmail.com>
2021-11-09 12:27:38 +0000

    temporary finish

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,17 +1,44 @@
 class Public::CartItemsController < ApplicationController
   def index
-
+   @cart_items = Cartitem.all
   end
 
+　def create
+　  @item = Item.find(params{:id})
+　  if @item.save(item_params)
+　    redirect_to cart_items_path
+　  end
+　end
+
+
   def updated
+    @cart_item = CartItem.find(params[:id])
+    if @cart_item.update(cart_item_params)
+      redirect_to cart_items_path
+    end
   end
 
   def destroy
+    @cart_item = CartItem.find(params[:id])
+    if @cart_item.destroy
+      redirect_to cart_items_path
+    end
   end
 
   def destroy_all
+    @cart_items = CartItem.all
+    if @cart_items.destroy_all
+      redirect_to cart_items_patj
+    end
   end
+private
 
-  def create
-  end
+def item_params
+  params.require(:cart_item).permit(:amount)
+end
+
+def cart_item_params
+  params.require(:cart_item).permit(:amount)
+end
+
 end
